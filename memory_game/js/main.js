8792c5397a73d9464ceb6129b4dddbdd27a8691d
Michael Fang <Ekim@Michaels-MBP.fios-router.home>
2019-01-18 12:47:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,28 @@
 console.log("Up and running!");
 var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "";
+var cardFour = "";
 
+cardsInPlay.push('queen');{
 
+	console.log("User played queen");
+}
+cardsInPlay.push('king');{
 
+	console.log("User played king");
+}
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert ("You found a match!");
+	}
+
+	else {
+		alert ("Sorry, try again!");
+	}
+}

1d2f6ef60fa97175695e29414a01fd62a1d927f0
Michael Fang <Ekim@Michaels-MBP.fios-router.home>
2019-01-21 14:13:30 -0500

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,29 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards =[
+{ 
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -13,10 +36,15 @@ console.log("Sorry, try again.");
 
 
 var flipCard = function (cardId) {
-cardsInPlay.push(cards[cardId]);
-console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
 checkForMatch();
+
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
 }
+
 
 flipCard(0);
 flipCard(2);
+
